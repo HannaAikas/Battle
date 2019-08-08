d45766c48c75e254c0b411caa11018974cb32935
Hanna <hannaaikas@gmail.com>
2019-08-08 19:34:55 +0100

    Add form for user to input 2 player names

--- app.rb
+++ app.rb
@@ -4,7 +4,13 @@ class Battle < Sinatra::Base
   set :sessions, true
 
   get '/' do
-    'Testing infrastructure working!'
+    erb(:index)
+  end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb(:play)
   end
 
   # start the server if ruby file executed directly
