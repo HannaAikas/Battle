fba3f4250981b3ffa71a8d96ba3a5d61bff5e39a
Hanna <hannaaikas@gmail.com>
2019-08-09 14:32:10 +0100

    Add feature: Reduce player 2 HP by 10 when attacked

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,14 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :hp
+  DEFAULT_HP = 60
 
-  def initialize(name=nil)
+  def initialize(name=nil, hp=DEFAULT_HP)
     @name = name
+    @hp = hp
   end
+
+  def player_hit
+    @hp -= 10
+  end
+
 end
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,4 +4,9 @@ feature 'Attacking' do
     click_link 'Attack'
     expect(page).to have_content 'Dave attacked Mittens'
   end
+  scenario 'Player 2 HP reduces by 10 when attacked' do
+    sign_in_and_play
+    click_link 'Attack'
+    expect(page).to have_content "Mittens: 50 HP"
+  end
 end
